7e9cb5e6227ff0ce55fecfc2c58ca947f3590207
lserra-iov <lucio.serra@iovlabs.org>
2022-12-23 19:05:45 +0100

    WIP: Get BTC public keys

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,36 @@ const BRIDGE = bridge.build(new Web3(MAINNET_URL))
 
 function App() {
   const [federationSize, setFederationSize] = useState(0)
+  const [btcPubKeys, setBtcPubKeys] = useState([])
 
-  useEffect(() => {
-    BRIDGE.methods
+  function getFederationSize() {
+    return BRIDGE.methods
       .getFederationSize()
       .call()
       .then((size: number) => setFederationSize(size))
+  }
+
+  async function getBtcPublicKeys() {
+    const requests = []
+    for (let fed = 0; fed < federationSize; fed++) {
+      requests.push(BRIDGE.methods.getFederatorPublicKeyOfType(fed, 'btc').call())
+    }
+    await Promise.all(requests).then((responses) => setBtcPubKeys(responses))
+  }
+
+  useEffect(() => {
+    getFederationSize().then(getBtcPublicKeys)
   }, [])
 
   return (
     <div>
       <h1>PowPeg Dashboard</h1>
       <span>Federation size: {federationSize}</span>
+      <ul>
+        {btcPubKeys.map((key) => (
+          <li key={key}>{key}</li>
+        ))}
+      </ul>
     </div>
   )
 }
