055cf5014d7d83a73b4fa1d170b17da25d857d71
lserra-iov <lucio.serra@iovlabs.org>
2022-12-23 17:58:39 +0100

    Get federation size

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import './App.css'
+import Web3 from 'web3'
+import { bridge } from '@rsksmart/rsk-precompiled-abis'
+import { useEffect, useState } from 'react'
+
+const MAINNET_URL = 'https://public-node.rsk.co'
+const BRIDGE = bridge.build(new Web3(MAINNET_URL))
 
 function App() {
-  return <span>PowPeg Dashboard</span>
+  const [federationSize, setFederationSize] = useState(0)
+
+  useEffect(() => {
+    BRIDGE.methods
+      .getFederationSize()
+      .call()
+      .then((size: number) => setFederationSize(size))
+  }, [])
+
+  return (
+    <div>
+      <h1>PowPeg Dashboard</h1>
+      <span>Federation size: {federationSize}</span>
+    </div>
+  )
 }
 
 export default App
