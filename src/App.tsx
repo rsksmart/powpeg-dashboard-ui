531b4416249b69a3f6ae3daac395e500fde10850
lserra-iov <lucio.serra@iovlabs.org>
2022-12-26 16:11:22 +0100

    Refactor get rskj data

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,42 @@
 import './App.css'
-import Web3 from 'web3'
-import { bridge } from '@rsksmart/rsk-precompiled-abis'
 import { useEffect, useState } from 'react'
-
-const MAINNET_URL = 'https://public-node.rsk.co'
-const BRIDGE = bridge.build(new Web3(MAINNET_URL))
+import { getBtcPublicKeys, getRskPublicKeys } from './services/getPublicKeys'
 
 function App() {
-  const [federationSize, setFederationSize] = useState(0)
-  const [btcPubKeys, setBtcPubKeys] = useState([])
-
-  // function getFederationSize() {
-  //   return BRIDGE.methods
-  //     .getFederationSize()
-  //     .call()
-  //     .then((size: number) => setFederationSize(size))
-  // }
-
-  async function getBtcPublicKeys() {
-    BRIDGE.methods
-      .getFederationSize()
-      .call()
-      .then((size: number) => {
-        setFederationSize(size)
-        const requests = []
-        for (let fed = 0; fed < size; fed++) {
-          requests.push(BRIDGE.methods.getFederatorPublicKeyOfType(fed, 'btc').call())
-        }
-        Promise.all(requests)
-          .then((responses) => setBtcPubKeys(responses))
-          .catch((e) => console.log(e))
-      })
-  }
+  const [btcPubKeys, setBtcPubKeys] = useState<string[]>([])
+  const [rskPubKeys, setRskPubKeys] = useState<string[]>([])
 
   useEffect(() => {
-    getBtcPublicKeys().catch((e) => console.log(e))
+    getBtcPublicKeys()
+      .then((keys) => setBtcPubKeys(keys))
+      .catch((err) => console.log(err))
+    getRskPublicKeys()
+      .then((keys) => setRskPubKeys(keys))
+      .catch((err) => console.log(err))
   }, [])
 
   return (
-    <div>
-      <h1>PowPeg Dashboard</h1>
-      <span>Federation size: {federationSize}</span>
-      <ul>
-        {btcPubKeys.length > 0 ? (
-          btcPubKeys.map((key) => <li key={key}>{key}</li>)
-        ) : (
-          <li>Loading...</li>
-        )}
-      </ul>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th colSpan={3}>PowPeg Dashboard</th>
+        </tr>
+        <tr>
+          <th>Index</th>
+          <th>Bitcoin public key</th>
+          <th>RSK public key</th>
+        </tr>
+      </thead>
+      <tbody>
+        {btcPubKeys.map((btcKey, index) => (
+          <tr key={btcKey}>
+            <td>{index + 1}</td>
+            <td>{btcKey}</td>
+            <td>{rskPubKeys[index]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
