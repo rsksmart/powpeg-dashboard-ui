dbe53c381394781bda86d5fb0a0f53d46e90582c
lserra-iov <lucio.serra@iovlabs.org>
2023-01-03 20:35:26 +0100

    Add a more meaningful test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,28 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
 import App from '../App';
+import { rskAddressFromPublicKey } from '../utils/rskAddressFromPublicKey';
 
-test('renders content', () => {
-  const component = render(<App />);
-  expect(component.getByText('Loading...')).toBeDefined();
+vi.mock('../services/getPublicKeys', () => ({
+  getBtcPublicKeys: vi
+    .fn()
+    .mockResolvedValue(['0x020ace50bab1230f8002a0bfe619482af74b338cc9e4c956add228df47e6adae1c']),
+  getRskPublicKeys: vi
+    .fn()
+    .mockResolvedValue(['0x0305a99716bcdbb4c0686906e77daf8f7e59e769d1f358a88a23e3552376f14ed2']),
+}));
+
+const mockedAddress =
+  '0x' +
+  rskAddressFromPublicKey('0x0305a99716bcdbb4c0686906e77daf8f7e59e769d1f358a88a23e3552376f14ed2');
+
+test('renders content', async () => {
+  render(<App />);
+  expect(
+    await screen.findByText('0x020ace50bab1230f8002a0bfe619482af74b338cc9e4c956add228df47e6adae1c')
+  ).toBeInTheDocument();
+  expect(
+    await screen.findByText('0x0305a99716bcdbb4c0686906e77daf8f7e59e769d1f358a88a23e3552376f14ed2')
+  ).toBeInTheDocument();
+  expect(await screen.findByText(mockedAddress)).toBeInTheDocument();
 });
